ca62551796ee85cfe945a1155a55e86f36b50f74
winterbitia <amoes.noland@gmail.com>
2024-05-03 11:02:04 +0700

    function template for paddock

---
+++ soal_3/actions.c
@@ -0,0 +1,34 @@
+#include <string.h>
+
+char* gap(float distance_now){
+    if (distance_now > 10)
+        return "Stay out of trouble";
+    if (distance_now > 3.5)
+        return "Push";
+        return "Gogogo";
+}
+
+char* fuel(float fuel_now){
+    if (fuel_now > 0.8)
+        return "Push Push Push";
+    if (fuel_now > 0.5)
+        return "You can go";
+        return "Conserve Fuel";
+}
+
+char* tire(int tire_now){
+    if (tire_now > 80)
+        return "Go Push Go Push";
+    if (tire_now > 50)
+        return "Good Tire Wear";
+    if (tire_now > 30)
+        return "Conserve Your Tire";
+        return "Box Box Box";
+}
+
+char* tire_change(char* type_now){
+    if (strcmp(type_now, "Soft") == 0)
+        return "Mediums Ready";
+    if (strcmp(type_now, "Medium") == 0)
+        return "Box for Softs";
+}
