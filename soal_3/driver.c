477e266ddfe22a98c25f3001a69f882b79860ee3
winterbitia <amoes.noland@gmail.com>
2024-05-04 22:46:46 +0700

    driver.c - beautiful formatting

--- soal_3/driver.c
+++ soal_3/driver.c
@@ -6,51 +6,75 @@
 
 /*  
     Soal_3 driver.c
-    VERSION 1 - barely exits
+    VERSION 2 - beautiful formatting
     Amoes Noland 5027231028
 */
 
+// Global definitions
 #define IP         "127.0.0.1"
 #define PORT       8080
 #define MAX_BUFFER 1024
 
+// I'm addicted to formatting
+void line(){
+    printf("-------------------------------------\n");
+}
+
+// Main series of functions
 int main() {
+    // Socket variables
     int client_socket;
     struct sockaddr_in server_address;
 
+    // Create client socket
     if ((client_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("Socket creation failed");
         exit(EXIT_FAILURE);
     }
 
+    // Configure server address
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(PORT);
 
+    // Address conversion for communication
     if (inet_pton(AF_INET, IP, &server_address.sin_addr) <= 0) {
         perror("Invalid address/ Address not supported");
         exit(EXIT_FAILURE);
     }
 
+    // Connect to server from client
     if (connect(client_socket, (struct sockaddr *)&server_address,
                                 sizeof(server_address)) < 0) {
         perror("Connection failed");
         exit(EXIT_FAILURE);
     }
 
-    printf("Connected to address %s:%d\n", inet_ntoa(server_address.sin_addr),
-                                           ntohs(server_address.sin_port));
+    // DEBUGGING
+    // printf("Connected to address %s:%d\n", inet_ntoa(server_address.sin_addr),
+    //                                        ntohs(server_address.sin_port));
 
+    // Define work variables
     char buffer[MAX_BUFFER],
          response[MAX_BUFFER];
 
+    // Initial message
+    printf("[CONNECTED TO DRIVER ASSISTANT]\n"); line();
+    printf("Here are the available commands:\n");
+    printf("  Gap        (float)  units\n");
+    printf("  Fuel       (float)  percentage\n");
+    printf("  Tire       (int)    usage\n");
+    printf("  TireChange (string) Soft/Medium\n");
+    printf("  Exit\n"); line();
+    printf("~ GOOD LUCK ON THE RACE! ~\n");
+
     while(1){
-        printf("\n[DRIVER ASSISTANT]\n(COMMANDS: Gap, Fuel, Tire, TireChange, Exit)\n\nDriver  : ");
+        printf("\nInput Driver   : ");
         memset(buffer, 0, MAX_BUFFER);
         fgets(buffer, MAX_BUFFER, stdin);
         buffer[strcspn(buffer, "\n")] = '\0';
 
         if (strcmp(buffer, "Exit") == 0){
-            printf("Client exited");
+            printf("Output Driver  : Client exited");
             return 0;
         }
 
@@ -68,9 +92,10 @@ int main() {
             break;
         }
 
-        printf("Paddock : %s\n", response);
+        printf("Output Paddock : %s\n", response);
     }
 
+    // Close client socket on disconnect
     close(client_socket);
     return 0;
 }
