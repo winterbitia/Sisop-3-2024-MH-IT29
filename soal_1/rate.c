b5d0873fc1f7ef7bc2b7d433d8ed47f5b891573c
winterbitia <amoes.noland@gmail.com>
2024-05-06 00:40:43 +0700

    rate.c - rating successful

--- soal_1/rate.c
+++ soal_1/rate.c
@@ -3,21 +3,27 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/stat.h>
-#include <sys/wait.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <dirent.h>
-#include <pthread.h>
 
 /*  
     Soal_1 rate.c
-    VERSION 1 - infrastructure
+    VERSION 2 - rating success
     Amoes Noland 5027231028
 */
 
 // Define global variables
 #define MAX_BUFFER 1028
+char dir_entry[MAX_BUFFER];
 int keygen;
+
+// Define comparison variables
+char max_t_name[MAX_BUFFER],
+     max_p_name[MAX_BUFFER],
+     max_t_fname[MAX_BUFFER],
+     max_p_fname[MAX_BUFFER];
+float max_t = 0, max_p = 0;
 
 // Function to get maximum key number from auth
 void keygen_get(){
@@ -31,7 +37,7 @@ void keygen_get(){
     keygen = *keygen_max;
 
     // DEBUGGING
-    printf("max: %d\n", keygen);
+    // printf("max: %d\n", keygen);
 
     // Detach variable
     shmdt(keygen_max);
@@ -41,13 +47,38 @@ void keygen_get(){
 }
 
 void csv_parse(char* filename){
-    return; 
+    // Open the selected file
+    chdir(dir_entry);
+    FILE *file = fopen(filename, "r");
+    if (!file) return;
+
+    // Ignore csv header
+    fscanf(file, "%*s");
+    // Start parsing data
+    // Ignore trailing newline and parse commas
+    char name[MAX_BUFFER]; float rating;
+    while (fscanf(file, "%*c%[^,],%f", name, &rating) == 2){
+        // Already authenticated, which means
+        // if not trashcan, guaranteed parking lot
+        if (strstr(filename, "_trashcan.csv") != NULL){
+            if (rating < max_t) continue;
+            max_t = rating;
+            strcpy(max_t_name, name);
+            strcpy(max_t_fname, filename);
+        } else {
+            if (rating < max_p) continue;
+            max_p = rating;
+            strcpy(max_p_name, name);
+            strcpy(max_p_fname, filename);
+        }
+    }
+    fclose(file);
 }
 
 // Function to grab csv filenames from auth
 void csv_get(){
     // DEBUGGING
-    printf("key: %d\n", keygen);
+    // printf("key: %d\n", keygen);
 
     // Generate new shared memory ID
     key_t key = (key_t)keygen--;
@@ -57,7 +88,10 @@ void csv_get(){
     char *shm_buffer = (char*)shmat(shmid, NULL, 0);
 
     // DEBUGGING
-    printf("buf: %s\n", shm_buffer);
+    // printf("buf: %s\n", shm_buffer);
+
+    // Send buffer to parse
+    csv_parse(shm_buffer);
 
     // Detach buffer to prepare next ID
     shmdt(shm_buffer);
@@ -66,9 +100,30 @@ void csv_get(){
     // shmctl(shmid, IPC_RMID, NULL);
 }
 
+void rating(char* type, char* fname, char* name, float rating){
+    printf("Type: %s\n"
+           "Filename: %s\n"
+           "--------------------\n"
+           "Name: %s\n"
+           "Rating: %.1f\n\n",
+           type, fname, name, rating);
+}
+
 int main(){
+    // Chdir will guarantee new-entry
+    chdir("../new-entry"); chdir("new-entry");
+    getcwd(dir_entry, sizeof(dir_entry));
+
+    // DEBUGGING
+    // printf("dir: %s\n", dir_entry);    
+
+    // Start main parsing process
     keygen_get();
     while (keygen > 1000) csv_get();
+
+    // Get final output
+    rating("Trash Can"  ,max_t_fname,max_t_name,max_t);
+    rating("Parking Lot",max_p_fname,max_p_name,max_p);
 
     return 0;
 }
